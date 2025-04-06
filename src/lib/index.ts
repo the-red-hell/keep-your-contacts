267a6bfe5123d67dead4a7ff7a84723a7b549bf8
p4xxus <79471967+p4xxus@users.noreply.github.com>
2025-04-06 13:22:58 +0200

    initial setup for the database & API endpoint

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,14 @@
 // place files you want to import through the `$lib` alias in this folder.
+export let put = async (note: String) => {
+    const response = await fetch("http://localhost:8000/todos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ note }),
+    });
+    if (!response.ok) {
+        throw new Error("Network response was not ok");
+    }
+    return await response.json();
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,75 @@
-<script>
-    import init, { main_js } from "$lib/pkg/backend";
+<script lang="ts">
+    import { put } from "$lib/index";
+    import { page } from "$app/state";
     import { onMount } from "svelte";
-
-    onMount(async () => {
-        await init().then(() => main_js());
+    let list = [
+        {
+            name: "John",
+            known_from: "kpmg",
+            position: "intern",
+            city: "Berlin",
+        },
+        {
+            name: "Doe",
+            known_from: "university",
+            position: "student",
+            city: "Munich",
+        },
+        {
+            name: "Alice",
+            known_from: "conference",
+            position: "manager",
+            city: "Hamburg",
+        },
+        {
+            name: "Bob",
+            known_from: "workshop",
+            position: "developer",
+            city: "Frankfurt",
+        },
+        {
+            name: "Eve",
+            known_from: "meetup",
+            position: "designer",
+            city: "Stuttgart",
+        },
+    ];
+    onMount(() => {
+        console.log(put("test"));
     });
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
+<div class="columnContainer">
+    <div class="rowContainer">
+        {#each Object.keys(list[0]) as header}
+            <h1>{header}</h1>
+        {/each}
+    </div>
+    {#each list as person, index}
+        <div class="rowContainer">
+            <p>{index}</p>
+            <p>{person.name}</p>
+            <p>{person.known_from}</p>
+            <p>{person.position}</p>
+            <p>{person.city}</p>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .columnContainer {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        height: 100vh;
+        width: 90vw;
+        flex-direction: column;
+    }
+    .rowContainer {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-direction: row;
+        width: 80vw;
+    }
+</style>
