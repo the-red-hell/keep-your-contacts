57995a20a330bd7b0125120de8292d7833a1d1ab
p4xxus <79471967+p4xxus@users.noreply.github.com>
2025-04-10 20:55:06 +0200

    you can see your location on the map now

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -1,15 +1,32 @@
-<script>
+<script lang="ts">
     import { Map, TileLayer, Marker, Popup } from "sveaflet";
+    let userLocation: [number, number] = $state([0, 0]);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                userLocation = [
+                    position.coords.latitude,
+                    position.coords.longitude,
+                ];
+                console.log("User location:", userLocation);
+            },
+            (error) => {
+                console.error("Error getting location:", error);
+            },
+        );
+    } else {
+        console.error("Geolocation is not supported by this browser.");
+    }
 </script>
 
 <div style="width:100%;height:100vh;">
     <Map
         options={{
-            center: [51.505, -0.09],
+            center: userLocation,
             zoom: 13,
         }}
     >
         <TileLayer url={"https://tile.openstreetmap.org/{z}/{x}/{y}.png"} />
-        <Marker latLng={[51.505, -0.09]} />
+        <Marker latLng={userLocation} />
     </Map>
 </div>
