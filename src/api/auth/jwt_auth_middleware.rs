176934aa104fad580dd39659cf786fded86665c5
p4xxus <79471967+p4xxus@users.noreply.github.com>
2025-04-22 20:47:26 +0200

    using TypedHeader for header extraction

--- src/api/auth/jwt_auth_middleware.rs
+++ src/api/auth/jwt_auth_middleware.rs
@@ -5,7 +5,11 @@ use axum::{
     middleware::Next,
     response::IntoResponse,
 };
-use axum_extra::extract::CookieJar;
+use axum_extra::{
+    extract::CookieJar,
+    headers::{self, authorization::Bearer},
+    TypedHeader,
+};
 use jsonwebtoken::{decode, DecodingKey, Validation};
 
 use crate::api::{errors::Error, MyState};
@@ -17,22 +21,15 @@ use super::{TokenClaims, User};
 pub async fn auth(
     cookie_jar: CookieJar,
     state: State<MyState>,
+    authorization_header: Option<TypedHeader<headers::Authorization<Bearer>>>,
     mut req: Request<Body>,
     next: Next,
 ) -> Result<impl IntoResponse, Error> {
-    // Extract token from cookie or Bearer header, TypedHeader not used since auth-header isn't required
+    // Extract token from cookie or Bearer header
     let token = cookie_jar
         .get("token")
         .map(|cookie| cookie.value().to_string())
-        .or_else(|| {
-            req.headers()
-                .get(header::AUTHORIZATION)
-                .and_then(|auth_header| auth_header.to_str().ok())
-                .and_then(|auth_value| match auth_value.starts_with("Bearer ") {
-                    true => Some(auth_value[7..].to_owned()),
-                    false => None,
-                })
-        });
+        .or_else(|| authorization_header.map(|auth_header| auth_header.token().to_string()));
 
     // Return error if token is not found
     let token = token.ok_or_else(|| Error::NotLoggedIn)?;
