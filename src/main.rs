3798bf145b5b364de4e6d642d9180ff8bdf0c4cb
p4xxus <79471967+p4xxus@users.noreply.github.com>
2025-05-01 21:51:15 +0200

    trying to put api online

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,7 @@ async fn main(
         ))
         .with(tracing_subscriber::fmt::layer())
         .init();
+    let _ = sqlx::query("DROP TABLE persons").execute(&pool).await;
     sqlx::query(
     "CREATE TABLE IF NOT EXISTS persons (id serial PRIMARY KEY, first_name TEXT NOT NULL, last_name TEXT, city TEXT NOT NULL, job TEXT, note TEXT)")
             .execute(&pool).await.expect("Failed to create table");
