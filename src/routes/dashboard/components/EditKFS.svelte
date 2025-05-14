45b03c40e447536ef1cf9c69d7cb09fe1fdf1f0b
p4xxus <79471967+p4xxus@users.noreply.github.com>
2025-05-14 14:15:24 +0200

    editing Known-From-Sources now works

--- src/routes/dashboard/components/EditKFS.svelte
+++ src/routes/dashboard/components/EditKFS.svelte
@@ -2,23 +2,35 @@
 	import { Modal } from "@skeletonlabs/skeleton-svelte";
 	import { knownFromSources } from "../store";
 	import { api_request } from "$lib";
-	import { ArrowLeft, ArrowRight } from "@lucide/svelte";
+	import { error } from "@sveltejs/kit";
 
 	let { kfsIds }: { kfsIds: number[] } = $props();
 
-	let kfss = $knownFromSources.filter((kfs) =>
-		kfsIds.includes(kfs.sourceId),
+	let kfss = $derived(
+		$knownFromSources.filter((kfs) =>
+			kfsIds.includes(kfs.sourceId),
+		),
 	);
 	let openState = $state(false);
 	function modalClose() {
 		openState = false;
 	}
 
-	function updateKfs() {
-		api_request(fetch, "", { method: "POST" }).then(async (res) => {
-			console.log(await res.json());
+	function updateKfs(sourceId: number, idx: number) {
+		api_request(fetch, `/known-from-sources/${sourceId}`, {
+			method: "PUT",
+			body: JSON.stringify({
+				sourceName: kfss[idx].sourceName,
+				description: kfss[idx].description,
+				locationSearch: kfss[idx].locationSearch,
+			}),
+		}).then(async (res) => {
+			if (!res.ok) error(500, await res.text());
+			knownFromSources.update((oldKfss) => {
+				return [...oldKfss, kfss[idx]];
+			});
 		});
-		modalClose();
+		console.log(kfss[idx]);
 	}
 </script>
 
@@ -30,7 +42,7 @@
 			type="text"
 			placeholder={label}
 			name={key}
-			value={kfss[idx][key]}
+			bind:value={kfss[idx][key]}
 		/>
 	</label>
 {/snippet}
@@ -50,7 +62,8 @@
 	{#snippet content()}
 		<header>
 			<h2 class="h2">
-				Edit {kfsIds.length === 1
+				Edit <br />
+				{kfsIds.length === 1
 					? '"' + kfss[0].sourceName + '"'
 					: "Known-From-Sources"}
 			</h2>
@@ -62,7 +75,7 @@
 				class="snap-x snap-mandatory scroll-smooth flex gap-2 pb-2 overflow-x-auto"
 			>
 				<!-- Loop through our array of knownFromSources. -->
-				{#each kfss as _kfs, idx}
+				{#each kfss as { sourceId }, idx}
 					<div
 						class="snap-center shrink-0 card py-20 px-4 w-60 xs:w-80 text-center flex flex-col gap-4"
 					>
@@ -81,20 +94,25 @@
 							"locationSearch",
 							idx,
 						)}
+						<button
+							type="button"
+							class="btn preset-filled"
+							onclick={() =>
+								updateKfs(
+									sourceId,
+									idx,
+								)}
+							>Update</button
+						>
 					</div>
 				{/each}
 			</div>
 		</article>
-		<footer class="flex justify-end gap-4">
+		<footer class="flex justify-end-safe">
 			<button
 				type="button"
-				class="btn preset-tonal"
-				onclick={modalClose}>Cancel</button
-			>
-			<button
-				type="button"
-				class="btn preset-filled"
-				onclick={updateKfs}>Confirm</button
+				class="btn preset-filled-surface-500 w-xs"
+				onclick={modalClose}>Done</button
 			>
 		</footer>
 	{/snippet}
